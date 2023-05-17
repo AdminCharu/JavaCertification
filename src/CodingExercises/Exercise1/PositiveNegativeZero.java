67209b45a0a9d31b92db1583f8352b5faeab7f64
AdminCharu <132778423+AdminCharu@users.noreply.github.com>
2023-05-18 00:32:07 +0530

    Add files via upload

---
+++ src/CodingExercises/Exercise1/PositiveNegativeZero.java
@@ -0,0 +1,23 @@
+package CodingExercises.Exercise1;
+
+public class PositiveNegativeZero {
+    public static void main(String[] args) {
+      /*  Positive, Negative or Zero
+        Write a method called checkNumber with an int parameter number.
+                The method should not return any value, and it needs to print out:
+
+        "positive" if the parameter number is > 0
+
+        "negative" if the parameter number is < 0
+
+        "zero" if the parameter number is equal to 0
+
+        NOTE: The checkNumber method needs to be defined as public static like we have been doing so far in the course.
+
+        NOTE: Do not add a main method to your solution code!*/
+    }
+//In the udemy IDE I am writing only this function
+    public static void checkNumber(int number) {
+        System.out.println((number > 0 ? "positive" : (number < 0) ? "negative" : "zero"));
+    }
+}
---
+++ src/CodingExercises/Exercise32/WallAreaMain.java
@@ -0,0 +1,8 @@
+package CodingExercises.Exercise32;
+
+public class WallAreaMain {
+    public static void main(String[] args) {
+        WallArea area=new WallArea(14,13);
+        System.out.println("Area of wall is : "+ area.getArea());
+    }
+}
